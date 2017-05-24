738126918f03fbaded93b912e95e26febc9f9255
lingirlsea <lingirlsea@126.com>
2017-05-24 12:07:33 +0800

    remove some code

--- tools.js
+++ tools.js
@@ -7,39 +7,12 @@
  */
 
 var tools = {
-	/**
-	 * 点击页面中的某个元素时，鼠标将滚到对应元素所在的位置
-	 * @param  targetID {String} [目标元素ID，#id]
-	 * @param  fromTop  {Number} [距顶部的距离]
-	 * @param  time     {Number} [时间，毫秒，默认300]
-	 */
-	scrollToElem: function(targetID, fromTop, time) {
-		clearInterval(window.ACC25548C59CA2A);
-
-	  var top, scrollTop, range, step;
-	  
-	  top = $(targetID).offset().top + 11 - (fromTop || 0);
-	  scrollTop = $(document).scrollTop();
-	  range = scrollTop - top;
-	  
-	  window.ACC25548C59CA2A = 1;
-	  window.ACC25548C59CA2A = setInterval(function() {
-	    step = range >= 0 ? Math.floor(range/10) : Math.ceil(range/10);
-	    range -= step;
-	    if(range === 10 || range === -10) {
-	      clearInterval(window.ACC25548C59CA2A);
-	    } else {
-	      $(document).scrollTop(range + top);
-	    }
-	  }, parseInt((time || 300) / 20) );
-	};
-
 	isIE: function(ver) {
 		var b = document.createElement('b');
     b.innerHTML = '<!--[if IE ' + ver + ']><i></i><![endif]-->';
     return b.getElementsByTagName('i').length === 1;
 	};
-	
+
 };
 
 
